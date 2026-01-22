dd4b9a989b9de0749ff0ce7c9a428d2e29579bb5
testinglife001 <testing.life001@gmail.com>
2026-01-22 08:42:42 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
@@ -11,6 +12,7 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
 
 
 
